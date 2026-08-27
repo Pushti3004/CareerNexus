e73a725cdb9cfa011c573804959ef90d2665cc4a
Pushti Gandhi <shreejipdg@gmail.com>
2026-08-27 16:12:55 +0530

    init

--- app/src/main/java/com/example/careernexus/LoginActivity.kt
+++ app/src/main/java/com/example/careernexus/LoginActivity.kt
@@ -1,28 +1,20 @@
 package com.example.careernexus
 
-import android.content.Intent
 import android.os.Bundle
-import android.os.Looper
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import java.util.logging.Handler
 
-class MainActivity : AppCompatActivity() {
+class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_login)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-        android.os.Handler(Looper.getMainLooper()).postDelayed({
-            val intent = Intent(this, LoginActivity::class.java)
-            intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or
-                    Intent.FLAG_ACTIVITY_CLEAR_TASK
-            startActivity(intent)
-        },1000)
     }
 }
