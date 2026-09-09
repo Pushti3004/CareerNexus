c266026a04b48d15f2278c499a224ad7fa3a4dde
Pushti Gandhi <shreejipdg@gmail.com>
2026-09-09 10:52:40 +0530

    init

--- app/src/main/java/com/example/careernexus/MainActivity.kt
+++ app/src/main/java/com/example/careernexus/MainActivity.kt
@@ -10,6 +10,8 @@ import androidx.core.view.WindowInsetsCompat
 import java.util.logging.Handler
 
 class MainActivity : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
