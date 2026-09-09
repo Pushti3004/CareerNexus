f25f163ec5a34bf6a98df7ecd579b7b48a4383d2
Pushti Gandhi <shreejipdg@gmail.com>
2026-09-09 19:07:07 +0530

    init

--- app/src/main/java/com/example/careernexus/Dashboard.kt
+++ app/src/main/java/com/example/careernexus/Dashboard.kt
@@ -1,28 +1,20 @@
 package com.example.careernexus
 
-import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class LoginActivity : AppCompatActivity() {
+class Dashboard : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_login)
+        setContentView(R.layout.activity_dashboard)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
-        }
-        explicitIntent()
-    }
-    fun explicitIntent() {
-        findViewById<Button>(R.id.btn_login).setOnClickListener {
-            Intent(this, Dashboard::class.java).also { startActivity(it) }
         }
     }
 }
--- app/src/main/java/com/example/careernexus/MainActivity.kt
+++ app/src/main/java/com/example/careernexus/MainActivity.kt
@@ -3,6 +3,8 @@ package com.example.careernexus
 import android.content.Intent
 import android.os.Bundle
 import android.os.Looper
+import android.view.animation.AnimationUtils
+import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -15,16 +17,23 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val logo = findViewById<ImageView>(R.id.CareerNexus)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        val logoAnimation = AnimationUtils.loadAnimation(
+            this,
+            R.anim.logo_animation
+        )
+        logo.startAnimation(logoAnimation)
         android.os.Handler(Looper.getMainLooper()).postDelayed({
             val intent = Intent(this, LoginActivity::class.java)
             intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or
                     Intent.FLAG_ACTIVITY_CLEAR_TASK
             startActivity(intent)
-        },1000)
+        },3000)
     }
 }
